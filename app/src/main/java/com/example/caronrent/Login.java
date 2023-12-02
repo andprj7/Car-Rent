241b0cdcdab63a23943a17da54e4e99d5572719a
Devang-Sarvaiya <devangsarvaiya99091@gmail.com>
2023-12-02 15:42:41 +0530

    Registration of Customer

--- app/src/main/java/com/example/caronrent/Login.java
+++ app/src/main/java/com/example/caronrent/Login.java
@@ -2,13 +2,30 @@ package com.example.caronrent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.ClosedSubscriberGroupInfo;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    public TextView txt_regis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txt_regis = findViewById(R.id.txt_regis);
+
+        txt_regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, SignUp.class));
+            }
+        });
+
+
     }
 }

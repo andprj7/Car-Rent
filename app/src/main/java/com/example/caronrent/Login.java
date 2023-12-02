0956cd85b9834b5a4af1ad3bea18a270a1376251
RAMANI MEET <meetgramani6697@gmail.com>
2023-12-02 21:58:17 +0530

    MEET RAMANI

--- app/src/main/java/com/example/caronrent/Login.java
+++ app/src/main/java/com/example/caronrent/Login.java
@@ -2,13 +2,27 @@ package com.example.caronrent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn=findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Login.this,Loc.class);
+                startActivity(i);
+            }
+        });
     }
 }
